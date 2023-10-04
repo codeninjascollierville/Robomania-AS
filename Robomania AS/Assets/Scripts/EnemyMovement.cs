6e6b6432978cf5af9791502d9971a1f587255844
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-10-04 17:48:46 -0400

    fixed debugs

--- Robomania AS/Assets/Scripts/EnemyMovement.cs
+++ Robomania AS/Assets/Scripts/EnemyMovement.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-{
+
       public float speed;
-}
+
   
     // Start is called before the first frame update
    // void Start()
    private void FixedUpdate()
   
-  
-  
-  
    {
         float newXPosition = transform.position.x + speed * Time.deltaTime;
-        float newYPosition = transform.position.y;(newXPosition, newYPosition);
-        Vector 2 newPosition = new Vector2
-        transform.position = newXPosition;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+        transform.position = newPosition;
     }
+}
 
     // Update is called once per frame
     //void Update()
-    {
+    
         
-    }
-}
+    
+
